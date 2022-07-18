75d2cf7080de83bf052beaec54536aee6542ad52
Joel Ruben Cauti Rojas <jcauti@globokas.com>
2022-07-18 17:41:15 -0500

    actualizacion

--- index.php
+++ index.php
@@ -59,7 +59,8 @@
                 )
             );
             array_push($res,$rw);
+            $respuesta = array("tasas" => $res)
         }
-        echo json_encode($res,JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+        echo json_encode($respuesta,JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
     }
 ?>
